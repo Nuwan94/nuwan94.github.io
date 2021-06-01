c884107795dcb572f57bd697d654d43561936788
Nuwan Alawatta <nuwansalawatta@gmail.com>
2021-06-01 20:41:30 +0530

    transfer to GitHub Actions

--- src/views/Portfolio/langIcon.js
+++ src/views/Portfolio/langIcon.js
@@ -1,279 +1,297 @@
-export const devIcons = [{
-        key: "angular",
-        name: "Angular",
-        icon: "nsa94-angular",
-    },
-    {
-        key: "android",
-        name: "Android",
-        icon: "nsa94-android",
-    },
-    {
-        key: "azure",
-        name: "Azure Cloud Platform",
-        icon: "nsa94-microsoftazure",
-    }, {
-        key: "azure-functions",
-        name: "Azure Functions",
-        icon: "nsa94-azurefunctions",
-    },
-    {
-        key: "azure-devops",
-        name: "Azure DevOps",
-        icon: "nsa94-azuredevops",
-    },
-    {
-        key: "bootstrap",
-        name: "Bootstrap",
-        icon: "nsa94-bootstrap",
-    }, {
-        key: "blender",
-        name: "Blender",
-        icon: "nsa94-blender",
-    },
-    {
-        key: "csharp",
-        name: "C#",
-        icon: "nsa94-csharp",
-    },
-    {
-        key: "css",
-        name: "CSS 3",
-        icon: "nsa94-css3",
-    },
-    {
-        key: "material",
-        name: "Material Design",
-        icon: "nsa94-materialdesign",
-    },
-    {
-        key: "dart",
-        name: "Dart",
-        icon: "nsa94-dart",
-    },
-    {
-        key: "docker",
-        name: "Docker",
-        icon: "nsa94-docker",
-    },
-    {
-        key: "express",
-        name: "Express",
-        icon: "nsa94-express",
-    },
-    {
-        key: "flutter",
-        name: "Flutter",
-        icon: "nsa94-flutter",
-    },
-    {
-        key: "git",
-        name: "git",
-        icon: "nsa94-git",
-    },
-    {
-        key: "github",
-        name: "GitHub",
-        icon: "fa fa-github",
-    },
-    {
-        key: "google-cloud",
-        name: "Google Cloud",
-        icon: "nsa94-googlecloud",
-    }, {
-        key: "google-play",
-        name: "Google Play",
-        icon: "nsa94-googleplay",
-    }, {
-        key: "hangouts",
-        name: "Google Hangouts",
-        icon: "nsa94-googlehangouts",
-    },
-    {
-        key: "go",
-        name: "Go",
-        icon: "nsa94-go",
-    },
-    {
-        key: "gradle",
-        name: "Gradle",
-        icon: "nsa94-gradle",
-    }, {
-        key: "graphql",
-        name: "GraphQL",
-        icon: "nsa94-graphql",
-    },
-    {
-        key: "heroku",
-        name: "Heroku",
-        icon: "nsa94-heroku",
-    },
-    {
-        key: "html",
-        name: "HTML 5",
-        icon: "nsa94-html5",
-    },
-    {
-        key: "java",
-        name: "Java",
-        icon: "nsa94-java",
-    },
-    {
-        key: "typescript",
-        name: "Typescript",
-        icon: "nsa94-typescript",
-    },
-    {
-        key: "javascript",
-        name: "JavaScript",
-        icon: "nsa94-javascript",
-    },
-    {
-        key: "less",
-        name: "LESS",
-        icon: "nsa94-less",
-    },
-    {
-        key: "mongodb",
-        name: "mongoDB",
-        icon: "nsa94-mongodb",
-    },
-    {
-        key: "mysql",
-        name: "MySQL",
-        icon: "nsa94-mysql",
-    },
-    {
-        key: "nodejs",
-        name: "Node.js",
-        icon: "nsa94-nodejs",
-    }, {
-        key: "nestjs",
-        name: "Nest.js",
-        icon: "nsa94-nestjs",
-    },
-    {
-        key: "php",
-        name: "PHP",
-        icon: "nsa94-php",
-    },
-    {
-        key: "postgresql",
-        name: "Postgresql",
-        icon: "nsa94-postgresql",
-    },
-    {
-        key: "python",
-        name: "Python",
-        icon: "nsa94-python",
-    },
-    {
-        key: "react",
-        name: "React",
-        icon: "nsa94-react",
-    },
-    {
-        key: "vuejs",
-        name: "Vue.js",
-        icon: "nsa94-vue-dot-js",
-    },
-    {
-        key: "vuetify",
-        name: "Vuetify",
-        icon: "nsa94-vuetify",
-    },
-    {
-        key: "gatsby",
-        name: "Gatsby",
-        icon: "nsa94-gatsby",
-    },
-    {
-        key: "travis",
-        name: "TravisCI",
-        icon: "nsa94-travisci",
-    },
-    {
-        key: "unity",
-        name: "Unity Engine",
-        icon: "nsa94-unity",
-    },
-    {
-        key: "markdown",
-        name: "Markdown",
-        icon: "nsa94-markdown",
-    },
-    {
-        key: "electron",
-        name: "Electron.js",
-        icon: "nsa94-electron",
-    },
-    {
-        key: "firebase",
-        name: "Firebase",
-        icon: "nsa94-firebase",
-    },
-    {
-        key: "deno",
-        name: "Deno",
-        icon: "nsa94-deno",
-    },
-    {
-        key: "socketio",
-        name: "Socket.io",
-        icon: "nsa94-socket-dot-io",
-    },
-    {
-        key: "jquery",
-        name: "jQuery",
-        icon: "nsa94-jquery",
-    },
-    {
-        key: "redux",
-        name: "Redux",
-        icon: "nsa94-redux",
-    },
-    {
-        key: "ant-design",
-        name: "Ant Design",
-        icon: "nsa94-ant-design",
-    },
-    {
-        key: "spring",
-        name: "Spring",
-        icon: "nsa94-spring",
-    },
-    {
-        key: "sonarqube",
-        name: "Sonarqube",
-        icon: "nsa94-sonarqube",
-    },
-    {
-        key: "kotlin",
-        name: "Kotlin",
-        icon: "nsa94-kotlin",
-    },
-    {
-        key: "postgresql",
-        name: "PostgreSQL",
-        icon: "nsa94-postgresql",
-    },
-    {
-        key: "apache-camel",
-        name: "Apache Camel",
-        icon: "nsa94-apache",
-    }, {
-        key: "twilio",
-        name: "Twilio",
-        icon: "nsa94-twilio",
-    },
-    {
-        key: "vuforia",
-        name: "Vuforia",
-        icon: "nsa94-vuforia",
-    },
-    {
-        key: "redis",
-        name: "Redis",
-        icon: "nsa94-redis",
-    }
+export const devIcons = [
+  {
+    key: "angular",
+    name: "Angular",
+    icon: "nsa94-angular",
+  },
+  {
+    key: "android",
+    name: "Android",
+    icon: "nsa94-android",
+  },
+  {
+    key: "azure",
+    name: "Azure Cloud Platform",
+    icon: "nsa94-microsoftazure",
+  },
+  {
+    key: "azure-functions",
+    name: "Azure Functions",
+    icon: "nsa94-azurefunctions",
+  },
+  {
+    key: "azure-devops",
+    name: "Azure DevOps",
+    icon: "nsa94-azuredevops",
+  },
+  {
+    key: "bootstrap",
+    name: "Bootstrap",
+    icon: "nsa94-bootstrap",
+  },
+  {
+    key: "blender",
+    name: "Blender",
+    icon: "nsa94-blender",
+  },
+  {
+    key: "csharp",
+    name: "C#",
+    icon: "nsa94-csharp",
+  },
+  {
+    key: "css",
+    name: "CSS 3",
+    icon: "nsa94-css3",
+  },
+  {
+    key: "material",
+    name: "Material Design",
+    icon: "nsa94-materialdesign",
+  },
+  {
+    key: "dart",
+    name: "Dart",
+    icon: "nsa94-dart",
+  },
+  {
+    key: "docker",
+    name: "Docker",
+    icon: "nsa94-docker",
+  },
+  {
+    key: "express",
+    name: "Express",
+    icon: "nsa94-express",
+  },
+  {
+    key: "flutter",
+    name: "Flutter",
+    icon: "nsa94-flutter",
+  },
+  {
+    key: "git",
+    name: "git",
+    icon: "nsa94-git",
+  },
+  {
+    key: "github",
+    name: "GitHub",
+    icon: "fa fa-github",
+  },
+  {
+    key: "google-cloud",
+    name: "Google Cloud",
+    icon: "nsa94-googlecloud",
+  },
+  {
+    key: "google-play",
+    name: "Google Play",
+    icon: "nsa94-googleplay",
+  },
+  {
+    key: "google-play",
+    name: "Google Play",
+    icon: "nsa94-googleplay",
+  },
+  {
+    key: "hibernate",
+    name: "Hibernate",
+    icon: "nsa94-hibernate",
+  },
+  {
+    key: "hangouts",
+    name: "Google Hangouts",
+    icon: "nsa94-googlehangouts",
+  },
+  {
+    key: "go",
+    name: "Go",
+    icon: "nsa94-go",
+  },
+  {
+    key: "gradle",
+    name: "Gradle",
+    icon: "nsa94-gradle",
+  },
+  {
+    key: "graphql",
+    name: "GraphQL",
+    icon: "nsa94-graphql",
+  },
+  {
+    key: "heroku",
+    name: "Heroku",
+    icon: "nsa94-heroku",
+  },
+  {
+    key: "html",
+    name: "HTML 5",
+    icon: "nsa94-html5",
+  },
+  {
+    key: "java",
+    name: "Java",
+    icon: "nsa94-java",
+  },
+  {
+    key: "typescript",
+    name: "Typescript",
+    icon: "nsa94-typescript",
+  },
+  {
+    key: "javascript",
+    name: "JavaScript",
+    icon: "nsa94-javascript",
+  },
+  {
+    key: "less",
+    name: "LESS",
+    icon: "nsa94-less",
+  },
+  {
+    key: "mongodb",
+    name: "mongoDB",
+    icon: "nsa94-mongodb",
+  },
+  {
+    key: "mysql",
+    name: "MySQL",
+    icon: "nsa94-mysql",
+  },
+  {
+    key: "nodejs",
+    name: "Node.js",
+    icon: "nsa94-nodejs",
+  },
+  {
+    key: "nestjs",
+    name: "Nest.js",
+    icon: "nsa94-nestjs",
+  },
+  {
+    key: "php",
+    name: "PHP",
+    icon: "nsa94-php",
+  },
+  {
+    key: "postgresql",
+    name: "Postgresql",
+    icon: "nsa94-postgresql",
+  },
+  {
+    key: "python",
+    name: "Python",
+    icon: "nsa94-python",
+  },
+  {
+    key: "react",
+    name: "React",
+    icon: "nsa94-react",
+  },
+  {
+    key: "vuejs",
+    name: "Vue.js",
+    icon: "nsa94-vue-dot-js",
+  },
+  {
+    key: "vuetify",
+    name: "Vuetify",
+    icon: "nsa94-vuetify",
+  },
+  {
+    key: "gatsby",
+    name: "Gatsby",
+    icon: "nsa94-gatsby",
+  },
+  {
+    key: "travis",
+    name: "TravisCI",
+    icon: "nsa94-travisci",
+  },
+  {
+    key: "unity",
+    name: "Unity Engine",
+    icon: "nsa94-unity",
+  },
+  {
+    key: "markdown",
+    name: "Markdown",
+    icon: "nsa94-markdown",
+  },
+  {
+    key: "electron",
+    name: "Electron.js",
+    icon: "nsa94-electron",
+  },
+  {
+    key: "firebase",
+    name: "Firebase",
+    icon: "nsa94-firebase",
+  },
+  {
+    key: "deno",
+    name: "Deno",
+    icon: "nsa94-deno",
+  },
+  {
+    key: "socketio",
+    name: "Socket.io",
+    icon: "nsa94-socket-dot-io",
+  },
+  {
+    key: "jquery",
+    name: "jQuery",
+    icon: "nsa94-jquery",
+  },
+  {
+    key: "redux",
+    name: "Redux",
+    icon: "nsa94-redux",
+  },
+  {
+    key: "ant-design",
+    name: "Ant Design",
+    icon: "nsa94-ant-design",
+  },
+  {
+    key: "spring",
+    name: "Spring",
+    icon: "nsa94-spring",
+  },
+  {
+    key: "sonarqube",
+    name: "Sonarqube",
+    icon: "nsa94-sonarqube",
+  },
+  {
+    key: "kotlin",
+    name: "Kotlin",
+    icon: "nsa94-kotlin",
+  },
+  {
+    key: "postgresql",
+    name: "PostgreSQL",
+    icon: "nsa94-postgresql",
+  },
+  {
+    key: "apache-camel",
+    name: "Apache Camel",
+    icon: "nsa94-apache",
+  },
+  {
+    key: "twilio",
+    name: "Twilio",
+    icon: "nsa94-twilio",
+  },
+  {
+    key: "vuforia",
+    name: "Vuforia",
+    icon: "nsa94-vuforia",
+  },
+  {
+    key: "redis",
+    name: "Redis",
+    icon: "nsa94-redis",
+  },
 ];
